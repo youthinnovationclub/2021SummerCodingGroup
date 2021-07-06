6525f4fe3f3eb307fe573de66600c8e397324963
Youth Inno Lab <youthinnolab@gmail.com>
2021-07-06 12:22:58 -0400

    project finished by Max Peng
    project finished by Max Peng

---
+++ fathersdaygift/father-love-data.py
@@ -0,0 +1,28 @@
+questions = [
+  {
+    'question': "How often do you talk to your dad?",
+    'answers': [
+      {'answer': 'Every hour', 'love': 60},
+      {'answer': 'Every day', 'love': 40},
+      {'answer': 'Once a week', 'love': 20},
+      {'answer': 'Once a month', 'love': 0}
+      ]
+  },
+  {
+    'question': "Do you give your father a gift on father's day?",
+    'answers': [
+      {'answer': 'Yes', 'love': 50},
+      {'answer': 'No', 'love': 0}
+      ]
+  },
+  {
+    'question': "Mom or Dad?",
+    'answers': [
+      {'answer': 'Neither', 'love': 0},
+      {'answer': 'Mom', 'love': 0},
+      {'answer': 'Dad', 'love': 75},
+      {'answer': 'Both', 'love': 25},
+      ]
+  },
+
+  ]
